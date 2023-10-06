16bb0174ff688e58aac2e323d376c7c8de82d899
Olivier Blanc <olivier3lanc@gmail.com>
2023-10-06 17:11:56 +0200

    fix paths

--- _data/site.js
+++ _data/site.js
@@ -1,15 +1,6 @@
 module.exports = function() {
-    let url = 'http://192.168.1.54';
-    let baseurl = '/conversations/_site';
-    const currentEnv = process.env.MY_ENVIRONMENT || "development";
-    if (currentEnv == 'production') {
-        url = 'https://unrivaled-dango-fdd77b.netlify.app';
-        baseurl = ''
-    }
     return {
         environment: process.env.MY_ENVIRONMENT || "development",
-        url: url,
-        baseurl: baseurl,
         imgsrv: 'https://wsrv.nl/?&url=',
         imgroot: 'https://olivier3lanc.ovh/cockpit/storage/uploads'
     };
