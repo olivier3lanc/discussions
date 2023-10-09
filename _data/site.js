9b1f27c0c8a39ce15761539b316e67b5ab200b56
Olivier Blanc <olivier3lanc@gmail.com>
2023-10-09 22:18:58 +0200

    feat: init page creation

--- _data/site.js
+++ _data/site.js
@@ -3,7 +3,9 @@ module.exports = function() {
         environment: process.env.MY_ENVIRONMENT || "development",
         imgsrv: process.env.IMAGE_SERVER_ENDPOINT,
         imgroot: process.env.SOURCES_IMAGES_ROOT,
-        api: process.env.COCKPIT_API_KEY
+        api: process.env.COCKPIT_API_KEY,
+        theme_color_values: ["grey", "blue", "green", "orange", "red"],
+        alignment_values: ["left", "right"]
     };
 };
   
