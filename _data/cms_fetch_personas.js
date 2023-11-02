0432a4b4c2335a2bf5e8dcbf60e1d9f4fd5f6d6d
Olivier Blanc <olivier3lanc@gmail.com>
2023-11-02 21:25:45 +0100

    comment

--- _data/cms_fetch_personas.js
+++ _data/cms_fetch_personas.js
@@ -1,16 +1,16 @@
 // Fetch l'URL et permet d'utiliser le résultat du fetch avec la variable = nom du fichier
 // Par exemple cms_fetch_links.js est utilisable dans les templates avec la variable cms_fetch_links
-const EleventyFetch = require("@11ty/eleventy-fetch");
+// const EleventyFetch = require("@11ty/eleventy-fetch");
 
-module.exports = async function() {
-  /* This returns a promise */
-  return EleventyFetch(process.env.COCKPIT_ENDPOINT_PERSONAS, {
-      duration: "1s", // save for 1 sec
-      type: "json",    // we’ll parse JSON for you
-      fetchOptions: {
-        headers: {
-            "api-key": process.env.COCKPIT_API_KEY
-        }
-      }
-  });
-};
+// module.exports = async function() {
+//   /* This returns a promise */
+//   return EleventyFetch(process.env.COCKPIT_ENDPOINT_PERSONAS, {
+//       duration: "1s", // save for 1 sec
+//       type: "json",    // we’ll parse JSON for you
+//       fetchOptions: {
+//         headers: {
+//             "api-key": process.env.COCKPIT_API_KEY
+//         }
+//       }
+//   });
+// };
