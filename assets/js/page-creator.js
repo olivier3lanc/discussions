575594a9d63c210919666292749bed605637d06a
Olivier Blanc <olivier3lanc@gmail.com>
2023-11-04 18:46:56 +0100

    feat: commandes edit

--- assets/js/page-creator.js
+++ assets/js/page-creator.js
@@ -19,6 +19,7 @@ const pageCreator = {
             pageCreator.publish();
         },
         _onSubmitFormMessage: function(evt) {
+            console.log('submit message');
             evt.preventDefault();
             pageCreator.addMessage();
         },
@@ -31,8 +32,10 @@ const pageCreator = {
             pageCreator.el_message_text.focus();
         },
         _onClickRemovePersonaPreset: function(evt) {
+            console.log('_onClickRemovePersonaPreset');
             evt.preventDefault();
             evt.target.closest('div').remove();
+            pageCreator.save();
         },
         _fetchPageResponse: function(response, id) {
             console.log(response,id);
@@ -57,9 +60,6 @@ const pageCreator = {
     addMessage: function() {
         // Init message related fields
         pageCreator._formdata_d_message_fields = [];
-        if (pageCreator.el_message_form !== null) {
-            pageCreator.el_message_form.addEventListener('submit', this._handlers._onSubmitFormMessage);
-        }
         // Get form data
         pageCreator.formdata_d_message = new FormData(pageCreator.el_message_form);
         // Get all message fields names and store them into an array
@@ -92,25 +92,10 @@ const pageCreator = {
                 current_message[name] = sanitized_value;
             });
             this._messages.push(current_message);
-            // console.log(this._messages);
+            console.log(this._messages);
             this.buildPage();
             this.addPersonaPreset();
-            // Store on localStorage
-            if (this.localStorageAvailable()) {
-                const els_custom_personas_presets = document.querySelectorAll('.d_message_persona_custom_preset');
-                let custom_personas = [];
-                els_custom_personas_presets.forEach(function(el) {
-                    custom_personas.push({
-                        id: el.id,
-                        value: el.value
-                    })
-                });
-                const backup = {
-                    custom_personas: custom_personas,
-                    messages: this._messages
-                }
-                localStorage.setItem("discussion", JSON.stringify(backup));
-            }
+            this.save();
             pageCreator.el_message_text.value = '';
             pageCreator.el_message_persona_full_name.focus();
         } else {
@@ -178,20 +163,27 @@ const pageCreator = {
         return string.replace(/(<([^>]+)>)/gi, "");
     },
     buildPage: function() {
-        let markup = '';
+        let markup = `
+            <link rel="stylesheet" href="/assets/css/preview.css">
+        `;
         pageCreator._messages.forEach(function(mess, index) {
             markup += `
-                <p>
-                    ${pageCreator.stripHTML(mess.d_message_text)}<br>
-                    <cite>- ${pageCreator.stripHTML(mess.d_message_persona_full_name || mess.d_message_persona_preset)}</cite><br>
-                    <button onclick="window.parent.pageCreator.removeMessage('${index}')">remove</button>
+                <p data-message="" data-message_alignment="${mess.d_message_alignment}" data-message_variant="${mess.d_message_variant}">
+                    <span data-message_text="">${pageCreator.stripHTML(mess.d_message_text)}</span><br>
+                    <cite data-message_persona="">${pageCreator.stripHTML(mess.d_message_persona_full_name || mess.d_message_persona_preset)}</cite><br>
+                    <button class="cmd_remove_message" onclick="window.parent.pageCreator.removeMessage('${index}')">remove</button>
+                    <button class="cmd_edit_message" onclick="window.parent.pageCreator.editMessage('${index}')">edit</button>
                 </p>
             `;
         });
+        const el_previewer_document = this.el_previewer.contentWindow.document;
         // Insert messages markup
-        this.el_previewer.contentWindow.document.body.innerHTML = markup;
+        el_previewer_document.body.innerHTML = markup;
         // scroll to the last message
-        this.el_previewer.contentWindow.document.querySelector('p:last-child').scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+        // const el_last_message = el_previewer_document.querySelector('p:last-child');
+        // if (el_last_message !== null) {
+        //     el_last_message.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+        // }
     },
     localStorageAvailable: function() {
         let storage;
@@ -220,8 +212,13 @@ const pageCreator = {
         }
     },
     addPersonaPreset: function() {
-        const current_value = document.querySelector('input[name="d_message_persona_preset"]:checked').value;
-        if (current_value == 'new') {
+        const el_input_checked_persona = document.querySelector('input[name="d_message_persona_preset"]:checked');
+        const el_input_new_persona = document.querySelector('input[name="d_message_persona_preset"][value="new"]');
+        // If none radio selected, select 'new' by default
+        if (el_input_checked_persona == null) {
+            el_input_new_persona.checked = true;
+        }
+        if (el_input_new_persona.checked) {
             pageCreator.el_message_persona_full_name.disabled = false;
             const index = pageCreator.el_message_persona_presets_container.querySelectorAll(`input[type="radio"]`).length + 1;
             const raw_value = pageCreator.el_message_persona_full_name.value;
@@ -247,10 +244,11 @@ const pageCreator = {
                     class="d_message_persona_custom_preset"
                     onclick="pageCreator._handlers._onClickPersonaPreset()">
                 <label for="${id}">${value}</label>
-                <button class="c-pos m-absolute m-bottom-100"
+                <a  href="#!command-remove"
+                    class="c-pos m-absolute m-bottom-100"
                     onclick="pageCreator._handlers._onClickRemovePersonaPreset(event)">
                     remove
-                </button>
+                </a>
             </div>
         `;
     },
@@ -269,10 +267,88 @@ const pageCreator = {
             this.buildPage();
         }
     },
+    // Store on localStorage
+    save: function() {
+        console.log('saved');
+        if (this.localStorageAvailable()) {
+            const els_custom_personas_presets = document.querySelectorAll('.d_message_persona_custom_preset');
+            let custom_personas = [];
+            els_custom_personas_presets.forEach(function(el) {
+                custom_personas.push({
+                    id: el.id,
+                    value: el.value
+                })
+            });
+            const backup = {
+                custom_personas: custom_personas,
+                messages: this._messages
+            }
+            localStorage.setItem("discussion", JSON.stringify(backup));
+        }
+    },
+    // Clear localStorage
     clear: function() {
         if (this.localStorageAvailable()) {
             localStorage.removeItem('discussion');
+            location.reload();
         }
+    },
+    renderCmdInputRadio: function(data) {
+        return `
+            <label for="cmd_${data.index}_${data.field}_${data.value}">
+                <input type="radio" 
+                    ${data.checked == data.value ? 'checked' : ''}
+                    id="cmd_${data.index}_${data.field}_${data.value}" 
+                    name="cmd_${data.index}_${data.field}" 
+                    value="${data.value}">
+                ${data.value}
+            </label>
+        `;
+    },
+    // Edit message
+    editMessage: function(index) {
+        console.log('edit:',index);
+        const el_to_edit = pageCreator.el_previewer.contentWindow.document.querySelectorAll('[data-message]')[index];
+        const el_message = el_to_edit.querySelector('[data-message_text]');
+        const el_persona = el_to_edit.querySelector('[data-message_persona]');
+        el_message.contentEditable = true;
+        el_persona.contentEditable = true;
+        let commands_markup = '';
+        const parameters_to_adjust = ['alignment', 'variant'];
+        parameters_to_adjust.forEach(function(param_name) {
+            const current_value = el_to_edit.dataset['message_'+param_name];
+            let possible_values = [];
+            document.querySelectorAll(`[name="d_message_${param_name}"]`).forEach(function(el) {
+                possible_values.push(el.value);
+            });
+            possible_values.forEach(function(value) {
+                commands_markup += pageCreator.renderCmdInputRadio({
+                    index: index,
+                    field: param_name,
+                    value: value,
+                    checked: current_value
+                })
+            });
+        });
+        commands_markup += `<button onclick="window.parent.pageCreator.saveMessage(${index})">save</button>`;
+        el_to_edit.insertAdjacentHTML('beforeend', commands_markup);
+        el_to_edit.querySelector('.cmd_edit_message').remove();
+    },
+    // Save message
+    saveMessage: function(index) {
+        console.log('save:',index);
+        const el_to_edit = pageCreator.el_previewer.contentWindow.document.querySelectorAll('[data-message]')[index];
+        const el_new_message = el_to_edit.querySelector('[data-message_text]');
+        const el_new_persona = el_to_edit.querySelector('[data-message_persona]');
+        el_new_message.contentEditable = false;
+        el_new_persona.contentEditable = false;
+        const new_message = pageCreator.stripHTML(el_new_message.innerHTML);
+        const new_persona = pageCreator.stripHTML(el_new_persona.innerHTML);
+        pageCreator._messages[index]['d_message_persona_full_name'] = new_persona;
+        pageCreator._messages[index]['d_message_text'] = new_message;
+        pageCreator._messages[index]['d_message_alignment'] = el_to_edit.querySelector(`[name="cmd_${index}_alignment"]:checked`).value;
+        pageCreator._messages[index]['d_message_variant'] = el_to_edit.querySelector(`[name="cmd_${index}_variant"]:checked`).value;
+        pageCreator.buildPage();
     }
 };
 pageCreator.el_page_form.addEventListener('submit', pageCreator._handlers._onSubmitFormPage);
@@ -286,5 +362,14 @@ window.pageCreator = {
         index = parseInt(index);
         pageCreator._messages.splice(index, 1);
         pageCreator.buildPage();
+        pageCreator.save();
+    },
+    editMessage: function(index) {
+        index = parseInt(index);
+        pageCreator.editMessage(index);
+    },
+    saveMessage: function(index) {
+        index = parseInt(index);
+        pageCreator.saveMessage(index);
     }
 }
